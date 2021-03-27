e25f51a5d8aca229812dddb300a2327d49e672b4
Akshay Gupta <akshaygupta.us@gmail.com>
2021-03-27 16:47:37 -0400

    pdf scraping from website to txt is done, just need to delete  the pdf that is backed up to pdfs/

--- backend/pdf_scraper.py
+++ backend/pdf_scraper.py
@@ -1,20 +1,47 @@
 from pdfminer import high_level
-
+import uuid
+import requests
 
 class PDF():
+
+    def _construct_file_name(self):
+        #make a random UUID that will be the name of the file based on the host ID and current time
+        name = str(uuid.uuid1())
+        return 'pdfs/' + name + ".pdf"
+
     def _scrape_pdf(self):
-        if not self.link:
+        #make a random UUID that will be the name of the file based on the host ID and current time
+        filename = self._construct_file_name()
+
+        try:
+            r = requests.get(self.url, stream=True)
+        except:
             raise ValueError
 
-        pdf_file_obj = open(self.link, 'rb')
+        chunk_size = 2000
+        try:
+            with open(filename, 'wb') as fd:
+                for chunk in r.iter_content(chunk_size):
+                    fd.write(chunk)
+        except:
+            raise IOError
+        
+        return filename
 
-        extracted_text = high_level.extract_text(self.link, "")
+    def _load_pdf(self):
+        if not self.url:
+            raise ValueError
+
+        filename = self._scrape_pdf()
+        pdf_file_obj = open(filename, 'rb')
+
+        extracted_text = high_level.extract_text(filename, "")
         self.text = extracted_text
 
     def __init__(self, link):
-        self.link = link
+        self.url = link
         self.text = None
-        self._scrape_pdf()
+        self._load_pdf()
 
     def get_text(self):
         return self.text
@@ -22,5 +49,6 @@ class PDF():
 
 if __name__ == "__main__":
     path = '/Users/akshay/projects/helloworld/LAHacks21/test.pdf'
+    path = 'https://arxiv.org/pdf/2103.13916.pdf'
     x = PDF(path)
     print(x.get_text())

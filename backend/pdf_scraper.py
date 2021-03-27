146eb778201f9a903ec0b532477fe4dbdb2e4d9c
Akshay Gupta <akshaygupta.us@gmail.com>
2021-03-27 14:38:44 -0400

    pdf class gets text from pdf file, now need to download the file from a link

--- backend/pdf_scraper.py
+++ backend/pdf_scraper.py
@@ -1,26 +1,29 @@
-import PyPDF2
+#from refextract import extract_references_from_file
+from PyPDF2 import PdfFileReader
+
+from pdfminer import high_level
 
 
 class PDF():
-
-    def _pdf_to_text(pdf):
-        raise NotImplementedError
-
-    def _get_pdf():
-        raise NotImplementedError
-
-    def _scrape_link():
+    def _scrape_pdf(self):
         if not self.link:
             raise ValueError
+
+        pdf_file_obj = open(self.link, 'rb')
+
+        extracted_text = high_level.extract_text(self.link, "")
+        self.text = extracted_text
 
     def __init__(self, link):
         self.link = link
         self.text = None
-        self.pages = None
-        self._scrape_link()
+        self._scrape_pdf()
 
     def get_text(self):
         return self.text
 
-    def get_num_pages(self):
-        return self.pages
+
+if __name__ == "__main__":
+    path = '/Users/akshay/projects/helloworld/LAHacks21/test.pdf'
+    x = PDF(path)
+    print(x.get_text())

4ab4d59739f5fb7cd2ee2435d577050d2e8a66c3
Brandon H <2brandonh@gmail.com>
2021-03-27 23:06:29 -0700

    graph

---
+++ my-app/src/Pages/Home/Graph.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { runForceGraph } from "./Graphrun";
+import "./Graph.css";
+
+export function ForceGraph({data}) {
+  const containerRef = React.useRef(null);
+
+  React.useEffect(() => {
+    let destroyFn;
+
+    if (containerRef.current) {
+      const { destroy } = runForceGraph(containerRef.current, data);
+      destroyFn = destroy;
+    }
+
+    return destroyFn;
+  }, []);
+
+  return <div ref={containerRef} id="graphwrap" />;
+}

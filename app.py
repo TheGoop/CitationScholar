2d30a956968445036d5386637bcfc0026eeef4ee
Neil Vaishampayan <neiluv@icloud.com>
2021-03-28 09:58:23 -0700

    bugfix for cors

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import request
 from flask import jsonify
 from flask import Response
 from backend import graphAPI
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def index():

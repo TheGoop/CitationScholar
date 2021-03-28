76c442c01c2aad587e4e44663fe7ec9d877af86e
Yash Lala <yashlala@gmail.com>
2021-03-27 18:52:32 -0700

    Fix: Add missing documentation to `finder.py`.

--- backend/finder.py
+++ backend/finder.py
@@ -12,6 +12,12 @@ class Finder():
             self.scrapers.append(keyword_map[scraper]())
 
     def findPaper(citation):
+        """Given a citation, return a PDF link to its corresponding paper.
+
+        For now, `citation` is a keyword that arXiv can search by. In the
+        future, we will make this a more specific object.
+        """
+
         for scraper in self.scrapers:
             res = scraper.findPaper(citation)
             if res is not None:
